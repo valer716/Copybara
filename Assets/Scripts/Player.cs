b30524d26e63b8bfda0fa84776347198c6695d9a
zsedelypeter <zsedely.peter@gmail.com>
2026-04-05 16:55:50 +0200

    Sprite forog a játékossal, falon már nem ragadunk fent

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,42 +1,51 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{
-
+{   
+    private SpriteRenderer spriteRenderer;
+    private bool isFacingRight = true;
     public float moveSpeed = 5f;
     public float jumpForce = 10f;
     public bool isGrounded = false;
+    private float moveInput;
     private Rigidbody2D rb;
 
+
+    [SerializeField] private Collider2D bodyCollider;
+    [SerializeField] private Collider2D feetCollider;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float moveInput = Input.GetAxis("Horizontal");
-        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
-
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        moveInput = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded){
+            rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
         }
+        
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocityY);
+        Flip();
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Ground"))
         {
-            Vector3 normal = other.GetContact(0).normal;
-            if (normal == Vector3.up)
+            if(other.otherCollider == feetCollider)
             {
-                isGrounded = true;
+                Vector3 normal = other.GetContact(0).normal;
+                if (normal == Vector3.up)
+                {
+                    isGrounded = true;
+                }
             }
-            
         }
     }
 
@@ -44,7 +53,18 @@ public class Player : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Ground"))
         {
-            isGrounded = false;
+            if(other.otherCollider == feetCollider)
+            {
+                isGrounded = false;
+            }
+            
+        }
+    }
+
+    private void Flip(){
+        if (isFacingRight && moveInput < 0f || !isFacingRight && moveInput > 0f){
+            isFacingRight = !isFacingRight;
+            spriteRenderer.flipX = !spriteRenderer.flipX;
         }
     }
 }
