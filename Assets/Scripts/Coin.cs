c0ddbee0b4a348ae603519ef5adebe3bb3412ce6
Matteuszhrv <horvath.mate1891@gmail.com>
2026-06-22 19:36:36 +0200

    coin

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    //ez az event dolog egy ilyen jelzés vmi más objectnek, hogy történt valami, az invoke-kal hívjuk meg, ez csak a deklaráció, és a másik objektumot fel kell majd iratkoztatni
+    public static event Action CoinPickedUp;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        CoinPickedUp?.Invoke();
+        Destroy(gameObject);
+    }
+}
+
